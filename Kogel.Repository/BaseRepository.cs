dab04b52b0a06e9fdc0d9f8ad8852f2a33c95f8f
gouhan <gouhan@163.com>
2020-05-07 16:39:30 +0800

    Insert修改，非自增列string主键时不调用InsertIdentity

--- Kogel.Repository/BaseRepository.cs
+++ Kogel.Repository/BaseRepository.cs
@@ -15,236 +15,238 @@ using Kogel.Dapper.Extension.Extension;
 
 namespace Kogel.Repository
 {
-	public abstract class BaseRepository<T> : IBaseRepository<T>, IDisposable
-	{
-		/// <summary>
-		/// 
-		/// </summary>
-		internal RepositoryOptionsBuilder OptionsBuilder { get; }
+    public abstract class BaseRepository<T> : IBaseRepository<T>, IDisposable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        internal RepositoryOptionsBuilder OptionsBuilder { get; }
 
-		/// <summary>
-		/// 连接对象
-		/// </summary>
-		public IDbConnection Orm { get => OptionsBuilder?.Connection ?? throw new DapperExtensionException("请在 OnConfiguring 中配置Connection"); }
+        /// <summary>
+        /// 连接对象
+        /// </summary>
+        public IDbConnection Orm { get => OptionsBuilder?.Connection ?? throw new DapperExtensionException("请在 OnConfiguring 中配置Connection"); }
 
-		/// <summary>
-		/// 工作单元
-		/// </summary>
-		public IUnitOfWork UnitOfWork { get; set; }
+        /// <summary>
+        /// 工作单元
+        /// </summary>
+        public IUnitOfWork UnitOfWork { get; set; }
 
-		public BaseRepository()
-		{
-			RepositoryOptionsBuilder builder = new RepositoryOptionsBuilder();
-			OnConfiguring(builder);
-			this.OptionsBuilder = builder;
-			UnitOfWork = new UnitOfWork(this.OptionsBuilder.Connection);
+        public BaseRepository()
+        {
+            RepositoryOptionsBuilder builder = new RepositoryOptionsBuilder();
+            OnConfiguring(builder);
+            this.OptionsBuilder = builder;
+            UnitOfWork = new UnitOfWork(this.OptionsBuilder.Connection);
 
-			//codefirst
-			SyncStructure();
-		}
+            //codefirst
+            SyncStructure();
+        }
 
-		~BaseRepository()
-		{
-			Dispose();
-		}
+        ~BaseRepository()
+        {
+            Dispose();
+        }
 
-		/// <summary>
-		/// 配置连接信息
-		/// </summary>
-		/// <param name="connectionFactory"></param>
-		public abstract void OnConfiguring(RepositoryOptionsBuilder builder);
+        /// <summary>
+        /// 配置连接信息
+        /// </summary>
+        /// <param name="connectionFactory"></param>
+        public abstract void OnConfiguring(RepositoryOptionsBuilder builder);
 
-		public void Dispose()
-		{
-			if (UnitOfWork != null)
-				UnitOfWork.Dispose();
-		}
+        public void Dispose()
+        {
+            if (UnitOfWork != null)
+                UnitOfWork.Dispose();
+        }
 
-		/// <summary>
-		/// 获取查询对象
-		/// </summary>
-		/// <returns></returns>
-		public QuerySet<T> QuerySet()
-		{
-			return new QuerySet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), null);
-		}
+        /// <summary>
+        /// 获取查询对象
+        /// </summary>
+        /// <returns></returns>
+        public QuerySet<T> QuerySet()
+        {
+            return new QuerySet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), null);
+        }
 
-		/// <summary>
-		/// 获取查询对象
-		/// </summary>
-		/// <param name="transaction"></param>
-		/// <returns></returns>
-		public QuerySet<T> QuerySet(IDbTransaction transaction)
-		{
-			return new QuerySet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), transaction);
-		}
+        /// <summary>
+        /// 获取查询对象
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+        public QuerySet<T> QuerySet(IDbTransaction transaction)
+        {
+            return new QuerySet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), transaction);
+        }
 
-		/// <summary>
-		/// 获取编辑对象
-		/// </summary>
-		/// <returns></returns>
-		public CommandSet<T> CommandSet()
-		{
-			return new CommandSet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), null);
-		}
+        /// <summary>
+        /// 获取编辑对象
+        /// </summary>
+        /// <returns></returns>
+        public CommandSet<T> CommandSet()
+        {
+            return new CommandSet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), null);
+        }
 
-		/// <summary>
-		/// 获取编辑对象
-		/// </summary>
-		/// <param name="transaction"></param>
-		/// <returns></returns>
-		public CommandSet<T> CommandSet(IDbTransaction transaction)
-		{
-			return new CommandSet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), transaction);
-		}
+        /// <summary>
+        /// 获取编辑对象
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+        public CommandSet<T> CommandSet(IDbTransaction transaction)
+        {
+            return new CommandSet<T>(OptionsBuilder.Connection, OptionsBuilder.Provider.CreateNew(), transaction);
+        }
 
-		/// <summary>
-		/// 根据主键获取当前实体数据
-		/// </summary>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		public T FindById(object id)
-		{
-			var entityObject = EntityCache.QueryEntity(typeof(T));
-			if (string.IsNullOrEmpty(entityObject.Identitys))
-				throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
-			//设置参数
-			DynamicParameters param = new DynamicParameters();
-			param.Add(entityObject.Identitys, id);
-			return this.QuerySet()
-				.Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
-				.Get();
-		}
+        /// <summary>
+        /// 根据主键获取当前实体数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T FindById(object id)
+        {
+            var entityObject = EntityCache.QueryEntity(typeof(T));
+            if (string.IsNullOrEmpty(entityObject.Identitys))
+                throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
+            //设置参数
+            DynamicParameters param = new DynamicParameters();
+            param.Add(entityObject.Identitys, id);
+            return this.QuerySet()
+                .Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
+                .Get();
+        }
 
-		/// <summary>
-		/// 增加(并且返回自增主键到写入到实体中)
-		/// </summary>
-		/// <param name="entity"></param>
-		/// <returns></returns>
-		public int Insert(T entity)
-		{
-			var entityObject = EntityCache.QueryEntity(typeof(T));
-			//存在主键写入自增id
-			if (!string.IsNullOrEmpty(entityObject.Identitys))
-			{
-				//var id = this.CommandSet()
-				//   .InsertIdentity(entity);
-				////写入主键数据
-				//entityObject.EntityFieldList
-				//	.First(x => x.IsIdentity).PropertyInfo
-				//	.SetValue(entity, id);
-				//return 1;
-				return this.CommandSet()
-					.Insert(entity);
-			}
-			else
-			{
-				//不存在就直接返回影响行数
-				return this.CommandSet()
-					.Insert(entity);
-			}
-		}
+        /// <summary>
+        /// 增加(并且返回自增主键到写入到实体中)
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public int Insert(T entity)
+        {
+            #region delete 2020/05/07 为支持非自增列主键 
+            //var entityObject = EntityCache.QueryEntity(typeof(T));
+            //存在主键写入自增id
+            //if (!string.IsNullOrEmpty(entityObject.Identitys))
+            //{
+            //	var id = this.CommandSet()
+            //	   .InsertIdentity(entity);
+            //	//写入主键数据
+            //	entityObject.EntityFieldList
+            //		.First(x => x.IsIdentity).PropertyInfo
+            //		.SetValue(entity, id);
+            //	return 1;
+            //}
+            //else
+            //{
+            //	//不存在就直接返回影响行数
+            //	return this.CommandSet()
+            //		.Insert(entity);
+            //}
+            #endregion
+            return this.CommandSet()
+                        .Insert(entity);
+        }
 
-		/// <summary>
-		/// 删除(根据主键)
-		/// </summary>
-		/// <param name="id"></param>
-		/// <returns></returns>
-		public int Delete(object id)
-		{
-			var entityObject = EntityCache.QueryEntity(typeof(T));
-			if (string.IsNullOrEmpty(entityObject.Identitys))
-				throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
-			//设置参数
-			DynamicParameters param = new DynamicParameters();
-			param.Add(entityObject.Identitys, id);
-			return this.CommandSet()
-				.Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
-				.Delete();
-		}
+        /// <summary>
+        /// 删除(根据主键)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int Delete(object id)
+        {
+            var entityObject = EntityCache.QueryEntity(typeof(T));
+            if (string.IsNullOrEmpty(entityObject.Identitys))
+                throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
+            //设置参数
+            DynamicParameters param = new DynamicParameters();
+            param.Add(entityObject.Identitys, id);
+            return this.CommandSet()
+                .Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
+                .Delete();
+        }
 
-		/// <summary>
-		/// 修改
-		/// </summary>
-		/// <param name="entity"></param>
-		/// <returns></returns>
-		public int Update(T entity)
-		{
-			var entityObject = EntityCache.QueryEntity(typeof(T));
-			if (string.IsNullOrEmpty(entityObject.Identitys))
-				throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
-			//获取主键数据
-			var id = entityObject.EntityFieldList
-				.First(x => x.IsIdentity).PropertyInfo
-				.GetValue(entity);
-			//设置参数
-			DynamicParameters param = new DynamicParameters();
-			param.Add(entityObject.Identitys, id);
-			return this.CommandSet()
-				.Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
-				.Update(entity);
-		}
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public int Update(T entity)
+        {
+            var entityObject = EntityCache.QueryEntity(typeof(T));
+            if (string.IsNullOrEmpty(entityObject.Identitys))
+                throw new DapperExtensionException("主键不存在!请前往实体类使用[Identity]特性设置主键。");
+            //获取主键数据
+            var id = entityObject.EntityFieldList
+                .First(x => x.IsIdentity).PropertyInfo
+                .GetValue(entity);
+            //设置参数
+            DynamicParameters param = new DynamicParameters();
+            param.Add(entityObject.Identitys, id);
+            return this.CommandSet()
+                .Where($"{entityObject.Identitys}={OptionsBuilder.Provider.ProviderOption.ParameterPrefix}{entityObject.Identitys}", param)
+                .Update(entity);
+        }
 
-		/// <summary>
-		/// 是否同步过实体
-		/// </summary>
-		private static bool IsFirstSyncStructure = false;
+        /// <summary>
+        /// 是否同步过实体
+        /// </summary>
+        private static bool IsFirstSyncStructure = false;
 
-		/// <summary>
-		/// 
-		/// </summary>
-		private static object AutoSyncLock = new object();
+        /// <summary>
+        /// 
+        /// </summary>
+        private static object AutoSyncLock = new object();
 
-		/// <summary>
-		/// 同步结构
-		/// </summary>
-		private void SyncStructure()
-		{
-			//用户配置是否需要同步
-			if (this.OptionsBuilder.IsAutoSyncStructure)
-			{
-				lock (AutoSyncLock)
-				{
-					if (!IsFirstSyncStructure)
-					{
-						//标记已经第一次同步
-						IsFirstSyncStructure = true;
-						//命名空间
-						string nameSpaces = string.Empty;
-						//类的完全限定名
-						string fullName = string.Empty;
-						switch (this.OptionsBuilder.Provider.GetType().Name)
-						{
-							case "MsSqlProvider":
-								{
-									nameSpaces = "Kogel.Dapper.Extension.MsSql";
-									fullName = "Kogel.Dapper.Extension.MsSql.Extension.CodeFirst";
-									break;
-								}
-							case "MySqlProvider":
-								{
-									nameSpaces = "Kogel.Dapper.Extension.MySql";
-									fullName = "Kogel.Dapper.Extension.MySql.Extension.CodeFirst";
-									break;
-								}
-							case "OracleSqlProvider":
-								{
-									nameSpaces = "Kogel.Dapper.Extension.Oracle";
-									fullName = "Kogel.Dapper.Extension.Oracle.Extension.CodeFirst";
-									break;
-								}
-						}
-						ICodeFirst codeFirst = (ICodeFirst)ReflectExtension.CreateInstance(nameSpaces, fullName, new object[] { Orm });
-						UnitOfWork.BeginTransaction(() =>
-						 {
-							 //开始同步实体
-							 codeFirst.SyncStructure();
-						 });
-						//提交（失败时会自动回滚）
-						UnitOfWork.Commit();
-					}
-				}
-			}
-		}
-	}
+        /// <summary>
+        /// 同步结构
+        /// </summary>
+        private void SyncStructure()
+        {
+            //用户配置是否需要同步
+            if (this.OptionsBuilder.IsAutoSyncStructure)
+            {
+                lock (AutoSyncLock)
+                {
+                    if (!IsFirstSyncStructure)
+                    {
+                        //标记已经第一次同步
+                        IsFirstSyncStructure = true;
+                        //命名空间
+                        string nameSpaces = string.Empty;
+                        //类的完全限定名
+                        string fullName = string.Empty;
+                        switch (this.OptionsBuilder.Provider.GetType().Name)
+                        {
+                            case "MsSqlProvider":
+                                {
+                                    nameSpaces = "Kogel.Dapper.Extension.MsSql";
+                                    fullName = "Kogel.Dapper.Extension.MsSql.Extension.CodeFirst";
+                                    break;
+                                }
+                            case "MySqlProvider":
+                                {
+                                    nameSpaces = "Kogel.Dapper.Extension.MySql";
+                                    fullName = "Kogel.Dapper.Extension.MySql.Extension.CodeFirst";
+                                    break;
+                                }
+                            case "OracleSqlProvider":
+                                {
+                                    nameSpaces = "Kogel.Dapper.Extension.Oracle";
+                                    fullName = "Kogel.Dapper.Extension.Oracle.Extension.CodeFirst";
+                                    break;
+                                }
+                        }
+                        ICodeFirst codeFirst = (ICodeFirst)ReflectExtension.CreateInstance(nameSpaces, fullName, new object[] { Orm });
+                        UnitOfWork.BeginTransaction(() =>
+                         {
+                             //开始同步实体
+                             codeFirst.SyncStructure();
+                         });
+                        //提交（失败时会自动回滚）
+                        UnitOfWork.Commit();
+                    }
+                }
+            }
+        }
+    }
 }
